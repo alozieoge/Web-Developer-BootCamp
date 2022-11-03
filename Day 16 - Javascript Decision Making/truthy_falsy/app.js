d63d379d698514271d46c7afbb583b40d7293d5e
alozieoge <39676713+alozieoge@users.noreply.github.com>
2022-11-03 20:12:06 +0000

    Add files via upload
    Upload HTML and Javascript files for truthy-falsy examples

---
+++ Day 16 - Javascript Decision Making/truthy_falsy/app.js
@@ -0,0 +1,50 @@
+// const userInput = prompt("Enter something");
+
+// if (userInput) {
+//     console.log("TRUTHY!");
+// }
+// else {
+//     console.log("FALSY!");
+// }
+
+// if (0) {
+//     console.log("TRUTHY!");
+// }
+// else {
+//     console.log("FALSY!");
+// }
+
+// if (null) {
+//     console.log("TRUTHY!");
+// }
+// else {
+//     console.log("FALSY!");
+// }
+
+// if (-123) {
+//     console.log("TRUTHY!");
+// }
+// else {
+//     console.log("FALSY!");
+// }
+
+// if (NaN) {
+//     console.log("TRUTHY!");
+// }
+// else {
+//     console.log("FALSY!");
+// }
+
+// if (undefined) {
+//     console.log("TRUTHY!");
+// }
+// else {
+//     console.log("FALSY!");
+// }
+
+if (' ') {
+    console.log("TRUTHY!");
+}
+else {
+    console.log("FALSY!");
+}
